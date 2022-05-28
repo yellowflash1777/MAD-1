864c8f8a06c6b6f23b9975a8a32c2500602ab498
Prashant Prithyani <prithyanipd@rknec.edu>
2022-05-28 19:26:28 +0530

    completed

--- Lab4/app.py
+++ Lab4/app.py
@@ -24,36 +24,50 @@ app=Flask(__name__)
 @app.route("/", methods=["GET","POST"])
 def index():
     if request.method=="GET":
-        return render_template("index.html")
-    elif request.method=="POST":
-        ID=request.form["ID"]
-        value=request.form["id_value"]
-        if not ID or not value:
-            return render_template("inv.html")
+            
 
-        elif ID=="course_id":
+            return render_template("index.html")
+
+
+
+      
+    elif request.method=="POST":
+        
+        ID=request.form.get("ID",None)
+        value=request.form["id_value"]
+        if request.form["id_value"] == "" or request.form.get("ID") ==None:
+            
+
+            return render_template("inv.html")
+            
+        
+            
+
+        if ID=="course_id":
             course_marks=list()
             for i in data:
                 
                 if i["Course id"]==value:
                     course_marks.append(int(i["Marks"]))
             if len(course_marks)==0:
-                    return render_template("inv.html")
+                
+
+                return render_template("inv.html")
 
    
             marks={'avg':mean(course_marks),'max':max(course_marks)}
             plt.clf()
             plt.hist(course_marks)
-            plt.savefig('static\saved.png') 
+            plt.savefig('static/saved.png') 
 
-            template_file=open("templates\course.html")
+            template_file=open("templates/course.html")
             TEMPLATE=template_file.read()
             template_file.close()
 
             template=Template(TEMPLATE)
             context=template.render(Marks=marks)
 
-            new_html=open("templates\output.html",'w')
+            new_html=open("templates/output.html",'w')
             new_html.write(context)
             new_html.close()
 
@@ -65,7 +79,9 @@ def index():
                     if i["Student id"]==value:
                         new_data.append(i)
                 if len(new_data)==0:
-                     return render_template("inv.html")
+                        
+
+                        return render_template("inv.html")
 
                 temp=list()
                 for i in new_data:
@@ -73,20 +89,21 @@ def index():
                 total_marks=sum(temp)
 
 
-                template_file=open("templates\student.html")
+                template_file=open("templates/student.html")
                 TEMPLATE=template_file.read()
                 template_file.close()
 
                 template=Template(TEMPLATE)
                 context=template.render(data=new_data,Total_Marks=total_marks)
 
-                new_html=open("templates\output.html",'w')
+                new_html=open("templates/output.html",'w')
                 new_html.write(context)
                 new_html.close()
 
                 return render_template("output.html")
     else :
-        return render_template("inv.html")
+
+            return render_template("inv.html")
 
 
 if __name__=="__main__":
