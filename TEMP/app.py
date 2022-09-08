9a3ef5701302b845f46b1d2be422d7ef61cbc14f
Prashant Prithyani <88495197+yellowflash1777@users.noreply.github.com>
2022-09-08 17:02:45 +0530

    Add files via upload

---
+++ TEMP/app.py
@@ -0,0 +1,12 @@
+from jinja2 import Template
+from flask import Flask,render_template,request
+
+app=Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("kanban.html")
+
+if __name__=="__main__":
+    app.debug=True
+    app.run()
