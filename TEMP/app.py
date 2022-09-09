76651a21a1f5f5d152064ebae00fb60be17e8fab
Prashant Prithyani <prithyanipd@rknec.edu>
2022-09-09 18:21:09 +0530

    .

--- TEMP/app.py
+++ TEMP/app.py
@@ -1,11 +1,59 @@
 from jinja2 import Template
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,redirect
+import sqlite3
+import os
 
 app=Flask(__name__)
 
+#currentlocation=os.path.dirname(os.path.dirpath(__file))
+
+
 @app.route("/")
-def index():
-    return render_template("kanban.html")
+def login():
+    return render_template("login.html")
+
+@app.route("/signup",methods=['GET','POST'])
+def signup():
+    if request.method=="POST":
+        
+        un=request.form.get('username')
+        pw=request.form.get('password')
+        #print(un,pw)
+        sqlconnection=sqlite3.Connection('newdb.sqlite3')
+        cursor=sqlconnection.cursor()
+
+        query1="insert into users(username,password) values ('{}','{}')".format(un,pw)
+        #print(query1)
+        cursor.execute(query1)
+        sqlconnection.commit()
+        cursor.close()
+
+        return redirect('/')
+
+    return render_template("signup.html")
+
+@app.route("/login_validation",methods=['POST'])
+def login_validation():
+    username=request.form.get('username')
+    password=request.form.get('password')
+
+    sqlconnection=sqlite3.Connection('newdb.sqlite3')
+    cursor=sqlconnection.cursor()
+    #print(username,password)
+    query0='SELECT username,password from users'
+
+    query1="SELECT username,password from users where username='{}' and password='{}'".format(username,password)
+    #print(query1)
+    rows=cursor.execute(query1)
+    rows=rows.fetchall()
+    cursor.close()
+    if len(rows)==1:
+        return render_template('kanban.html')
+    else:
+        return redirect('/')
+
+    
+    return rows
 
 if __name__=="__main__":
     app.debug=True
