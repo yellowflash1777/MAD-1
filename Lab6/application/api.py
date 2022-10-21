faba1c4da715996bf7bab3edb30fef50fd7827e7
Prashant Prithyani <prithyani2002@gmail.com>
2022-10-21 18:38:06 +0530

    created validation.py

--- Lab6/application/api.py
+++ Lab6/application/api.py
@@ -1,9 +1,24 @@
 from flask_restful import Resource
-from numpy import delete
+from flask_restful import marshal_with,fields
+from application.database import db
+from application.models import Course
+
+course_fields={
+    "course_id":fields.Integer,
+    "course_name":fields.String,
+    "course_code":fields.String,
+    "course_description":fields.String, 
+}
 
 class CourseAPI(Resource):
+
+    @marshal_with(course_fields)
     def get(self,course_id):
-        pass
+        course=db.session.query(Course).filter(Course.course_id==course_id).first()
+        if course:
+            return course
+        else:
+            raise NotFoundError(status_code=404)
 
     def put(self,course_id):
         pass
